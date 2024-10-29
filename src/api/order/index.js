a4b3f0348b7108c6e0339cdaac2b2e15a11021f6
zhangweizhen <zhang.weizhen2@ztesoft.com>
2024-10-29 23:25:38 +0800

    订单查询

--- src/api/order/index.js
+++ src/api/order/index.js
@@ -10,32 +10,32 @@ import request from '@/utils/request';
 export function useOrderApi() {
 	return {
 		list: (params) => {
-		return request({
-			url: '/order/order/list',
-			method: 'get',
-			params
-		});
-},
-	add: (params) => {
-		return request({
-							url: '/order/order/order',
+			return request({
+				url: '/order/list',
+				method: 'get',
+				params
+			});
+		},
+		add: (params) => {
+			return request({
+				url: '/order/save',
+				method: 'post',
+				data: params
+			});
+		},
+		update: (params) => {
+			return request({
+				url: '/order/update',
 				method: 'put',
-						data: params
-		});
-	},
-	update: (params) => {
-		return request({
-							url: '/order/order/order',
-				method: 'put',
-						data: params
-		});
-	},
-	delete: (params) => {
-		return request({
-							url: '/order/order/order',
+				data: params
+			});
+		},
+		delete: (params) => {
+			return request({
+				url: '/order/delete',
 				method: 'delete',
-						data: params
-		});
-	}
-};
+				data: params
+			});
+		}
+	};
 }
